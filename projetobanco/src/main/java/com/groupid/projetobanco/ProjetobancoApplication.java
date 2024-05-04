532c7301be09c9eae558b53c927df412467a0ffc
EstelaLacerda <elo@cesar.school>
2024-05-04 16:24:54 -0300

    Renascimento :o

---
+++ projetobanco/src/main/java/com/groupid/projetobanco/ProjetobancoApplication.java
@@ -0,0 +1,13 @@
+package com.groupid.projetobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobancoApplication.class, args);
+	}
+
+}
---
+++ projetobanco/src/test/java/com/groupid/projetobanco/ProjetobancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupid.projetobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
