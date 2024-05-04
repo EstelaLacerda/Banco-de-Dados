98e437d8b6c15fdf52a070ce2d027aae232220c7
camilacirne <camilacirne@hotmail.com>
2024-05-04 16:50:14 -0300

    informaçoes

--- projetobanco/src/main/java/com/groupid/projetobanco/models/class.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/models/class.java
@@ -2,4 +2,8 @@ package com.groupid.projetobanco.models;
 
 public class Class {
     // superclasse, gets and sets
+    // private String nome;
 }
+
+//criar uma função toString()
+//pucblic java.lang.String toString()  
--- projetobanco/src/main/java/com/groupid/projetobanco/repository/userRepository.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/repository/userRepository.java
@@ -2,4 +2,13 @@ package com.groupid.projetobanco.repository;
 
 public class UserRepository {
     // Comandos sql, update
+
+/* 
+@Repository
+public classs ProdutoRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate; //para usar comandos sql
+}
+*/
 }
