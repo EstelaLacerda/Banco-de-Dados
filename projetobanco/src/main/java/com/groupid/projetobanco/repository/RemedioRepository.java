eeacba2b62680493c65321fa4b36ecb5baa8dbb8
camilacirne <camilacirne@hotmail.com>
2024-05-27 20:09:42 -0300

    Remedio Repository Atualizado

--- projetobanco/src/main/java/com/groupid/projetobanco/repository/RemedioRepository.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/repository/RemedioRepository.java
@@ -14,63 +14,61 @@ public class RemedioRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public void updateRemedio(Remedio remedio){
-        jdbcTemplate.update("UPDATE INTO REMEDIO(CODIGO_REMEDIO, NOME, PRINCIPIO_ATIVO, TIPO, QUANTIDADE, UNIDADE_MEDIDA, DOSAGEM, COD_ESTOQUE, CONTROLADO) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        remedio.getCodigo(), remedio.getNomeRemedio(), remedio.getPrincipioAtivo(), remedio.getTipo(), remedio.getQuantidade(), 
-        remedio.getUnidadeMedida(), remedio.getDosagem(),remedio.getCodEstoque(), remedio.getControlado()
-        );
-    }
-
     public boolean insertRemedio(Remedio remedio) {
         int rowsAffected = jdbcTemplate.update(
-            "INSERT INTO REMEDIO(CODIGO_REMEDIO, NOME, PRINCIPIO_ATIVO, TIPO, QUANTIDADE, UNIDADE_MEDIDA, DOSAGEM, COD_ESTOQUE, CONTROLADO) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            remedio.getCodigo(), remedio.getNomeRemedio(), remedio.getPrincipioAtivo(), remedio.getTipo(), remedio.getQuantidade(), 
-            remedio.getUnidadeMedida(), remedio.getDosagem(),remedio.getCodEstoque(), remedio.getControlado()
+            "INSERT INTO REMEDIO(NOME, PRINCIPIO_ATIVO, TIPO, QUANTIDADE, UNIDADE_MEDIDA, DOSAGEM, COD_ESTOQUE, CONTROLADO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            remedio.getNomeRemedio(), remedio.getPrincipioAtivo(), remedio.getTipo(), remedio.getQuantidade(), 
+            remedio.getUnidadeMedida(), remedio.getDosagem(), remedio.getCodEstoque(), remedio.getControlado()
         );
         return rowsAffected > 0;
     }
 
-    public boolean deleteRemedio(int codigo){
-
-        int rowsAffected = jdbcTemplate.update("DELETE FROM REMEDIO WHERE CODIGO = ?", codigo);
+    public boolean deleteRemedio(int codigo) {
+        int rowsAffected = jdbcTemplate.update("DELETE FROM REMEDIO WHERE CODIGO_REMEDIO = ?", codigo);
         if (rowsAffected > 0) {
             System.out.println("Remédio e suas dependências foram excluídos com sucesso.");
         } else {
             System.out.println("Não foi possível excluir o Remédio ou não foi encontrado na base de dados.");
         }
-
-        return true;
-
+        return rowsAffected > 0;
     }
 
     public List<Remedio> getAllRemedios() {
         return jdbcTemplate.query("SELECT * FROM REMEDIO", (resultSet, rowNum) -> {
-            Remedio remedio = new Remedio(rowNum, null, null, null, null, null, null, rowNum, rowNum);
+            Remedio remedio = new Remedio(rowNum, null, null, null, rowNum, null, null, rowNum, false);
             remedio.setCodigo(resultSet.getInt("CODIGO_REMEDIO")); 
             remedio.setNomeRemedio(resultSet.getString("NOME"));    
             remedio.setPrincipioAtivo(resultSet.getString("PRINCIPIO_ATIVO")); 
             remedio.setTipo(resultSet.getString("TIPO"));
-            remedio.setQuantidade(resultSet.getString("QUANTIDADE"));
+            remedio.setQuantidade(resultSet.getInt("QUANTIDADE"));
             remedio.setUnidadeMedida(resultSet.getString("UNIDADE_MEDIDA"));
             remedio.setDosagem(resultSet.getString("DOSAGEM"));
             remedio.setCodEstoque(resultSet.getInt("COD_ESTOQUE"));
-            remedio.setControlado(resultSet.getInt("CONTROLADO"));
+            remedio.setControlado(resultSet.getBoolean("CONTROLADO"));
             return remedio;
         });
     }
 
+    public void updateRemedio(Remedio remedio) {
+        jdbcTemplate.update(
+            "UPDATE REMEDIO SET NOME = ?, PRINCIPIO_ATIVO = ?, TIPO = ?, QUANTIDADE = ?, UNIDADE_MEDIDA = ?, DOSAGEM = ?, COD_ESTOQUE = ?, CONTROLADO = ? WHERE CODIGO_REMEDIO = ?",
+            remedio.getNomeRemedio(), remedio.getPrincipioAtivo(), remedio.getTipo(), remedio.getQuantidade(), 
+            remedio.getUnidadeMedida(), remedio.getDosagem(), remedio.getCodEstoque(), remedio.getControlado(), remedio.getCodigo()
+        );
+    }
+
     public List<Remedio> getAllRemediosPrincipioAtivoClonazepam() {
         return jdbcTemplate.query("SELECT * FROM REMEDIO WHERE NOME IN (SELECT NOME_REMEDIO FROM ESTOQUE WHERE NOME_REMEDIO = 'Clonazepam')", (resultSet, rowNum) -> {
-            Remedio remedio = new Remedio(rowNum, null, null, null, null, null, null, rowNum, rowNum);
+            Remedio remedio = new Remedio(rowNum, null, null, null, rowNum, null, null, rowNum, false);
             remedio.setCodigo(resultSet.getInt("CODIGO_REMEDIO")); 
             remedio.setNomeRemedio(resultSet.getString("NOME"));    
             remedio.setPrincipioAtivo(resultSet.getString("PRINCIPIO_ATIVO")); 
             remedio.setTipo(resultSet.getString("TIPO"));
-            remedio.setQuantidade(resultSet.getString("QUANTIDADE"));
+            remedio.setQuantidade(resultSet.getInt("QUANTIDADE"));
             remedio.setUnidadeMedida(resultSet.getString("UNIDADE_MEDIDA"));
             remedio.setDosagem(resultSet.getString("DOSAGEM"));
             remedio.setCodEstoque(resultSet.getInt("COD_ESTOQUE"));
-            remedio.setControlado(resultSet.getInt("CONTROLADO"));
+            remedio.setControlado(resultSet.getBoolean("CONTROLADO"));
             return remedio;
         });
     }
