a09e7c8e2291bff307c2abf8abf4bfda17f32c72
camilacirne <camilacirne@hotmail.com>
2024-05-27 20:03:35 -0300

    Repository Receita Atualizada

--- projetobanco/src/main/java/com/groupid/projetobanco/repository/ReceitaRepository.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/repository/ReceitaRepository.java
@@ -3,7 +3,6 @@ package com.groupid.projetobanco.repository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,44 +15,46 @@ public class ReceitaRepository {
     private JdbcTemplate jdbcTemplate;
 
     public void insertReceita(Receita receita) {
-      
-        try {
-            String sql = "INSERT INTO receita(data_prescricao, id_receita, crm, quantidade, horario, dosagem, status_receita, hora_consulta, matricula_medico, cpf_paciente, nome_remedio) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            jdbcTemplate.update(sql, receita.getDataPrescricao(), receita.getIdReceita(), receita.getCrm(), receita.getQuantidade(),
-                            receita.getHorario(), receita.getDosagem(), receita.getStatusReceita(), receita.getHoraConsulta(),
-                            receita.getMatriculaFuncionarioMedico(), receita.getCpfPaciente(), receita.getNomeRemedio());
-        } catch (DataAccessException e) {
-            throw new RuntimeException("Erro ao inserir receita: " + e.getMessage(), e);
-        }
+        jdbcTemplate.update("INSERT INTO RECEITA (DATA_HORA_CONSULTA, CRM, MATRICULA_MEDICO, CODIGO_PACIENTE, NOME_REMEDIO, DOSAGEM, QUANTIDADE, UNIDADE_MEDIDA, HORARIO, RECEITA_CONTROLADA, STATUS_RECEITA) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            receita.getDataHoraConsulta(), receita.getCrm(), receita.getMatriculaFuncionarioMedico(), receita.getCodigoPaciente(), receita.getNomeRemedio(),
+            receita.getDosagem(), receita.getQuantidade(), receita.getUnidadeMedida(), receita.getHorario(), receita.getReceitacontrolada(), receita.getStatusReceita());
     }
     
     public boolean deleteReceita(int id_receita) {
-        int rowsAffected = jdbcTemplate.update("DELETE FROM Receita WHERE ID_RECEITA = ?", id_receita);
+        int rowsAffected = jdbcTemplate.update("DELETE FROM RECEITA WHERE ID_RECEITA = ?", id_receita);
     
         if (rowsAffected > 0) {
-            System.out.println("Receita e suas dependências foram excluídos com sucesso.");
+            System.out.println("Receita e suas dependências foram excluídas com sucesso.");
         } else {
-            System.out.println("Não foi possível excluir a Receita ou não encontrado na base de dados.");
+            System.out.println("Não foi possível excluir a Receita ou não foi encontrada na base de dados.");
         }
         
         return rowsAffected > 0;
     }
 
-    public List<Receita> getAllReceitas(){
+    public List<Receita> getAllReceitas() {
         return jdbcTemplate.query("SELECT * FROM RECEITA", (resultSet, rowNum) -> {
-            Receita receita = new Receita(null, rowNum, null, null, null, null, null, null, rowNum, null, null);
+            Receita receita = new Receita(rowNum, null, null, rowNum, rowNum, null, null, rowNum, null, null, null, false, null, null);
             receita.setIdReceita(resultSet.getInt("ID_RECEITA"));
-            receita.setDataPrescricao(resultSet.getDate("DATAPRESCRICAO"));
+            receita.setDataHoraConsulta(resultSet.getTimestamp("DATA_HORA_CONSULTA"));
             receita.setCrm(resultSet.getString("CRM"));
-            receita.setQuantidade(resultSet.getString("QUANTIDADE"));
-            receita.setHorario(resultSet.getString("HORARIO"));
+            receita.setMatriculaFuncionarioMedico(resultSet.getInt("MATRICULA_MEDICO"));
+            receita.setCodigoPaciente(resultSet.getInt("CODIGO_PACIENTE"));
+            receita.setNomeRemedio(resultSet.getString("NOME_REMEDIO"));
             receita.setDosagem(resultSet.getString("DOSAGEM"));
-            receita.setStatusReceita(resultSet.getString("STATUSRECEITA"));
-            receita.setHoraConsulta(resultSet.getTime("HORACONSULTA"));
-            receita.setMatriculaFuncionarioMedico(resultSet.getInt("MATRICULAFUNCIONARIOMEDICO"));
-            receita.setCpfPaciente(resultSet.getString("CPFPACIENTE"));
-            receita.setNomeRemedio(resultSet.getString("NOMEREMEDIO"));
+            receita.setQuantidade(resultSet.getInt("QUANTIDADE"));
+            receita.setUnidadeMedida(resultSet.getString("UNIDADE_MEDIDA"));
+            receita.setHorario(resultSet.getString("HORARIO"));
+            receita.setReceitaControlada(resultSet.getBoolean("RECEITA_CONTROLADA"));
+            receita.setStatusReceita(resultSet.getString("STATUS_RECEITA"));
             return receita;
         });
+    }
+
+    public boolean updateReceita(Receita receita) {
+        int rowsAffected = jdbcTemplate.update("UPDATE RECEITA SET DATA_HORA_CONSULTA = ?, CRM = ?, MATRICULA_MEDICO = ?, CODIGO_PACIENTE = ?, NOME_REMEDIO = ?, DOSAGEM = ?, QUANTIDADE = ?, UNIDADE_MEDIDA = ?, HORARIO = ?, RECEITA_CONTROLADA = ?, STATUS_RECEITA = ? WHERE ID_RECEITA = ?",
+            receita.getDataHoraConsulta(), receita.getCrm(), receita.getMatriculaFuncionarioMedico(), receita.getCodigoPaciente(), receita.getNomeRemedio(),
+            receita.getDosagem(), receita.getQuantidade(), receita.getUnidadeMedida(), receita.getHorario(), receita.getReceitacontrolada(), receita.getStatusReceita(), receita.getIdReceita());
+        return rowsAffected > 0;
     }
 }
