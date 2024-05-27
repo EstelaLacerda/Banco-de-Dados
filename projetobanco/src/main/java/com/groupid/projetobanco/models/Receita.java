cba19a1e96c835168888078065b3d9db9ba0f8b6
camilacirne <camilacirne@hotmail.com>
2024-05-27 13:00:04 -0300

    Receita Classe Atualizada

--- projetobanco/src/main/java/com/groupid/projetobanco/models/Receita.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/models/Receita.java
@@ -1,47 +1,47 @@
 package com.groupid.projetobanco.models;
 
+import java.security.Timestamp;
 import java.sql.Time;
 import java.util.Date;
 
 public class Receita {
-
-    private Date data_prescricao;
+    
     private int id_receita;
+    private Timestamp data_hora_consulta;
     private String crm;
-    private String quantidade;
-    private String horario;
-    private String dosagem;
-    private String status_receita;
-    private Time hora_consulta;
     private int matricula_medico;
-    private String cpf_paciente;
+    private int codigo_paciente;
     private String nome_remedio;
+    private String dosagem;
+    private int quantidade;
+    private String unidade_medida;
+    private String horario;
+    private Timestamp data_prescricao;
+    private boolean receita_controlada;
+    private String status_receita;
+    private String tempo_de_espera;
+    
 
-    public Receita(Date data_prescricao, int id_receita, String crm, String quantidade, 
-    String horario, String dosagem, String status_receita, Time hora_consulta, int matricula_medico, String cpf_paciente, String nome_remedio){
-        this.data_prescricao = data_prescricao;
-        this.id_receita = id_receita;
-        this.crm = crm;
-        this.quantidade = quantidade;
-        this.horario = horario;
-        this.dosagem = dosagem;
-        this.status_receita = status_receita;
-        this.hora_consulta = hora_consulta;
-        this.matricula_medico = matricula_medico;
-        this.cpf_paciente = cpf_paciente;
-        this.nome_remedio = nome_remedio;
+    public Receita(int id_receita, Timestamp data_hora_consulta, String crm, int matricula_medico, int codigo_paciente, String nome_remedio, 
+     String dosagem,int quantidade, String unidade_medida, String horario, Timestamp data_prescricao, boolean receita_controlada, String status_receita,
+     String tempo_de_espera){
+      this.id_receita = id_receita;
+      this.data_hora_consulta = data_hora_consulta;
+      this.crm = crm;
+      this.matricula_medico = matricula_medico;
+      this.codigo_paciente = codigo_paciente;
+      this.nome_remedio = nome_remedio;
+      this.dosagem = dosagem;
+      this.quantidade = quantidade;
+      this.unidade_medida = unidade_medida;
+      this.horario = horario;
+      this.data_prescricao = data_prescricao;
+      this.receita_controlada = receita_controlada;
+      this.status_receita = status_receita;
+      this.tempo_de_espera = tempo_de_espera;
+       
     }
 
-    // Getter e Setter Data de Prescrição
-    public Date getDataPrescricao() {
-        return data_prescricao;
-    }
-
-    public void setDataPrescricao(Date data_prescricao) {
-        this.data_prescricao = data_prescricao;
-    }
-
-    // Getter e Setter ID da Receita
     public int getIdReceita() {
         return id_receita;
     }
@@ -50,7 +50,14 @@ public class Receita {
         this.id_receita = id_receita;
     }
 
-    // Getter e Setter CRM
+    public Timestamp getDataHoraConsulta() {
+        return data_hora_consulta;
+    }
+
+    public void setDataHoraConsulta( Timestamp data_hora_consulta){
+        this.data_hora_consulta = data_hora_consulta;
+    }
+
     public String getCrm() {
         return crm;
     }
@@ -59,53 +66,8 @@ public class Receita {
         this.crm = crm;
     }
 
-    // Getter e Setter Quantidade
-    public String getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(String quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    // Getter e Setter Horário
-    public String getHorario() {
-        return horario;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
-    // Getter e Setter Dosagem
-    public String getDosagem() {
-        return dosagem;
-    }
-
-    public void setDosagem(String dosagem) {
-        this.dosagem = dosagem;
-    }
-
-    // Getter e Setter Status da Receita
-    public String getStatusReceita() {
-        return status_receita;
-    }
-
-    public void setStatusReceita(String status_receita) {
-        this.status_receita = status_receita;
-    }
-
-    // Getter e Setter Hora da Consulta
-    public Time getHoraConsulta() {
-        return hora_consulta;
-    }
-
-    public void setHoraConsulta(Time hora_consulta) {
-        this.hora_consulta = hora_consulta;
-    }
-
-    // Getter e Setter Matricula do Funcionario Medico
-    public int getMatriculaFuncionarioMedico() {
+     // Getter e Setter Matricula do Funcionario Medico
+     public int getMatriculaFuncionarioMedico() {
         return matricula_medico;
     }
 
@@ -113,13 +75,12 @@ public class Receita {
         this.matricula_medico = matricula_medico;
     }
 
-    // Getter e Setter FK_Paciente_CPF
-    public String getCpfPaciente() {
-        return cpf_paciente;
+    public int getCodigoPaciente(){
+        return codigo_paciente;
     }
 
-    public void setCpfPaciente(String cpf_paciente) {
-        this.cpf_paciente = cpf_paciente;
+    public void setCodigoPaciente(int codigo_paciente){
+        this.codigo_paciente = codigo_paciente;
     }
 
     public String getNomeRemedio() {
@@ -129,4 +90,70 @@ public class Receita {
     public void setNomeRemedio(String nome_remedio) {
         this.nome_remedio = nome_remedio;
     }
+
+    public String getDosagem() {
+        return dosagem;
+    }
+
+    public void setDosagem(String dosagem) {
+        this.dosagem = dosagem;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getUnidadeMedida() {
+        return unidade_medida;
+    }
+
+    public void setUnidadeMedida(String unidade_medida) {
+        this.unidade_medida = unidade_medida;
+    }
+    
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public Timestamp getDataPrescricao() {
+        return data_prescricao;
+    }
+
+    public void setDataPrescricao(Timestamp data_prescricao) {
+        this.data_prescricao = data_prescricao;
+    }
+
+    public boolean getReceitacontrolada(){
+        return receita_controlada;
+    }
+
+    public void setReceitaControlada(boolean receita_controlada){
+        this.receita_controlada = receita_controlada;
+    }
+
+    public String getStatusReceita() {
+        return status_receita;
+    }
+
+    public void setStatusReceita(String status_receita) {
+        this.status_receita = status_receita;
+    }
+
+    public String getTempoDeEspera() {
+        return tempo_de_espera;
+    }
+    
+    public void setTempoDeEspera( String tempo_de_espera){
+        this.tempo_de_espera = tempo_de_espera;
+    }
+  
+
 }
