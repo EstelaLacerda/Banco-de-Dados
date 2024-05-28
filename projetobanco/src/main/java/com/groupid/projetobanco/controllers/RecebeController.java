3124e782c67f3edd3ac038abf5644da5681b8e80
camilacirne <camilacirne@hotmail.com>
2024-05-27 21:22:00 -0300

    Recebe e Receita Controller

--- projetobanco/src/main/java/com/groupid/projetobanco/controllers/RecebeController.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/controllers/RecebeController.java
@@ -3,13 +3,7 @@ package com.groupid.projetobanco.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.groupid.projetobanco.models.Recebe;
 import com.groupid.projetobanco.repository.RecebeRepository;
@@ -27,16 +21,14 @@ public class RecebeController {
         return "Receita recebida pelo Funcionário!\n";
     }
     
-    @DeleteMapping("/{id_receita}")
-    public String deleteRecebe(@PathVariable int id_receita) {
-        boolean deleted = recebeRepository.deleteRecebe(id_receita);
+    @DeleteMapping("/{matricula_funcionario}/{id_receita}")
+    public String deleteRecebe(@PathVariable int matricula_funcionario, @PathVariable int id_receita) {
+        boolean deleted = recebeRepository.deleteRecebe(matricula_funcionario, id_receita);
         
         if (deleted) {
             return "Receita retirada do Funcionário!\n";
-        } 
-        
-        else {
-            return "A receita com esse id não está contida nesse Funcionário especificado no banco de dados!\n";
+        } else {
+            return "A receita com esse ID não está associada a esse funcionário especificado no banco de dados!\n";
         }
     }
 
--- projetobanco/src/main/java/com/groupid/projetobanco/controllers/ReceitaControleEspecialController.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/controllers/ReceitaControleEspecialController.java
@@ -3,44 +3,46 @@ package com.groupid.projetobanco.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.groupid.projetobanco.models.RemedioControlado;
+import com.groupid.projetobanco.models.Receita_controle_especial;
 import com.groupid.projetobanco.repository.ReceitaControleEspecialRepository;
 
 @RestController
 @RequestMapping("/remedio/controlado")
 public class ReceitaControleEspecialController {
+
     @Autowired
-    private ReceitaControleEspecialRepository remedioControladoRepository;
+    private ReceitaControleEspecialRepository receitaControleEspecialRepository;
 
     @PostMapping
-    public String createRemedioControlado(@RequestBody RemedioControlado remedio_controlado){
-        remedioControladoRepository.insertRemedioControlado(remedio_controlado);
-        return "Remédio controlado inserido!\n";
+    public String createReceitaControleEspecial(@RequestBody Receita_controle_especial receitaControleEspecial) {
+        receitaControleEspecialRepository.insertReceitaControleEspecial(receitaControleEspecial);
+        return "Receita controle especial inserida!\n";
     }
 
-    @DeleteMapping("/{fk_Remedio_codigo}")
-    public String deleteRemedioControlado(@PathVariable int codigo_remedio){
-        boolean deleted = remedioControladoRepository.deleteRemedioControlado(codigo_remedio);
-        if(deleted){
-            return "Remédio controlado deletado!\n";
+    @DeleteMapping("/{id_receita}")
+    public String deleteReceitaControleEspecial(@PathVariable int id_receita) {
+        boolean deleted = receitaControleEspecialRepository.deleteReceitaControleEspecial(id_receita);
+        if (deleted) {
+            return "Receita controle especial deletada!\n";
         } else {
-            return "O Remédio controlado com esse código não existe no banco de dados!\n";
+            return "A receita controle especial com esse ID não existe no banco de dados!\n";
         }
     }
 
-    @GetMapping
-    public List<RemedioControlado> getRemedioControlado(){
-        return remedioControladoRepository.getAllRemediosControlados();
+    @GetMapping("/allreceitacontroleespecial")
+    public List<Receita_controle_especial> getReceitasControleEspecial() {
+        return receitaControleEspecialRepository.getAllReceitasControleEspecial();
     }
 
+    @PutMapping("/{id_receita}")
+    public String updateReceitaControleEspecial(@PathVariable int id_receita, @RequestBody Receita_controle_especial receitaControleEspecial) {
+        boolean updated = receitaControleEspecialRepository.updateReceitaControleEspecial(receitaControleEspecial);
+        if (updated) {
+            return "Receita controle especial atualizada!\n";
+        } else {
+            return "Não foi possível atualizar a receita controle especial com esse ID!\n";
+        }
+    }
 }
-    
-
