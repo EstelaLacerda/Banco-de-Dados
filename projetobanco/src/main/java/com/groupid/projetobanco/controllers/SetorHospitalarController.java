fc24d036b908be82f033f661369a27864f230193
camilacirne <camilacirne@hotmail.com>
2024-05-27 21:37:01 -0300

    Setor Hospitalar Controller Atualizado

--- projetobanco/src/main/java/com/groupid/projetobanco/controllers/SetorHospitalarController.java
+++ projetobanco/src/main/java/com/groupid/projetobanco/controllers/SetorHospitalarController.java
@@ -1,6 +1,5 @@
 package com.groupid.projetobanco.controllers;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +19,17 @@ import com.groupid.projetobanco.repository.SetorHospitalarRepository;
 @RequestMapping("/setorhospitalar")
 public class SetorHospitalarController {
     @Autowired
-    private SetorHospitalarRepository SetorHospitalarRepository;
+    private SetorHospitalarRepository setorHospitalarRepository;
 
     @PostMapping
-    public String createSetorHospitalar(@RequestBody Setor_hospitalar Setor_hospitalar){
-        SetorHospitalarRepository.insertSetorHospitalar(Setor_hospitalar);
+    public String createSetorHospitalar(@RequestBody Setor_hospitalar setor_hospitalar){
+        setorHospitalarRepository.insertSetorHospitalar(setor_hospitalar);
         return "Setor Hospitalar inserido!\n";
     }
 
     @DeleteMapping("/{codigo_setor}")
     public String deleteSetorHospitalar(@PathVariable int codigo_setor) {
-        boolean deleted = SetorHospitalarRepository.deleteSetorHospitalar(codigo_setor);
+        boolean deleted = setorHospitalarRepository.deleteSetorHospitalar(codigo_setor);
         if(deleted){
             return "Setor Hospitalar deletado!\n";
         } else {
@@ -39,8 +39,17 @@ public class SetorHospitalarController {
 
     @GetMapping("/allsetorhospitalar")
     public List<Setor_hospitalar> getSetoreshospitalar(){
-        return SetorHospitalarRepository.getAllSetoresHospilar();
+        return setorHospitalarRepository.getAllSetoresHospitalar();
     }
 
+    @PutMapping("/atualizar/{codigo_setor}")
+    public String updateSetorHospitalar(@PathVariable int codigo_setor, @RequestBody Setor_hospitalar setor_hospitalar) {
+        setor_hospitalar.setCodigoSetor(codigo_setor);
+        boolean updated = setorHospitalarRepository.updateSetorHospitalar(setor_hospitalar);
+        if(updated){
+            return "Setor Hospitalar atualizado!\n";
+        } else {
+            return "Não foi possível atualizar o Setor Hospitalar com esse código!\n";
+        }
+    }
 }
-
